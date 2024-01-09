a34dab7e987c63a73b3bda1b3e3d9435dcb94547
Alberto Morabito <albert@cord.com>
2024-01-09 18:00:45 +0000

    Day 5: In the style of Vera Molnár (1924-2023)
    Summary:
    TIL who Vera Molnár was. She truly has lots of amazing work, here's my tribute.
    I like very symmetrical things, so I went for squares.
    Used some random number magic recently learned in a video by Frank Force and Dwitter!

---
+++ 5/index.js
@@ -0,0 +1,63 @@
+const MIN_MOLNAR_LEVEL = 5;
+const MAX_MOLNAR_LEVEL = 50;
+const SIZE = 200;
+
+let molnarLevel = MIN_MOLNAR_LEVEL;
+function setup() {
+  angleMode(DEGREES);
+  colorMode(HSB);
+  const canvas = createCanvas(innerWidth, innerHeight);
+  fill(0, 0, 0, 0);
+
+  molnar();
+
+  canvas.mouseClicked(() => {
+    clear();
+    molnarLevel =
+      molnarLevel > MAX_MOLNAR_LEVEL ? MIN_MOLNAR_LEVEL : molnarLevel + 5;
+    molnar({ randomizeColor: true });
+  });
+}
+
+function draw() {}
+
+function drawMolnarSquare(
+  x,
+  y,
+  size,
+  { randomizeColor } = { randomizeColor: false }
+) {
+  if (randomizeColor) {
+    stroke(Math.random() * 360, 80, 80);
+  }
+  // To make mobile a bit more interesting, choose the longer
+  // axis as the one that increases the rotation.
+  const longerAxis = Math.max(x, y);
+  const magicRotation = longerAxis / 20;
+  for (let i = 0; i < molnarLevel; i++) {
+    const magicNumber = Math.sin(Math.random() * 100);
+    const magicPosition = magicNumber * 20;
+    const magicSize = Math.abs(magicNumber * 30);
+    push();
+    // Good ol' translate to rotation point, rotate, then transform back.
+    translate(x + size / 2, y + size / 2);
+    rotate(magicRotation);
+    translate(-x - size / 2, -y - size / 2);
+
+    rect(x + magicPosition, y + magicPosition, size - i * magicSize);
+    pop();
+  }
+}
+
+function molnar({ randomizeColor } = { randomizeColor: false }) {
+  for (let row = 0; row < innerWidth; row += SIZE) {
+    for (let col = 0; col < innerHeight; col += SIZE) {
+      drawMolnarSquare(row, col, SIZE, { randomizeColor });
+    }
+  }
+}
+
+function windowResized() {
+  resizeCanvas(innerWidth, innerHeight);
+  molnar({ randomizeColor: true });
+}
